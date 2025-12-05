a73aa6b63265208815a5d22986b756b7d9261b82
Ruben Manrique <rmanrique@render.com>
2025-12-05 13:05:37 -0600

    add tasks

---
+++ index.js
@@ -0,0 +1,24 @@
+import { startTaskServer, task } from "@render/sdk/workflows"
+
+const square = task(
+  { name: "square" },
+  (num) => {
+    return num * num;
+  }
+)
+
+task(
+  { name: "addSquares" },
+  async (a, b) => {
+    const num1 = await square(a);
+    const num2 = await square(b);
+    return num1 + num2
+  }
+)
+
+startTaskServer()
+  .then(() =>{
+    console.log("task server started")
+  }).catch(e => {
+    console.log("an error happened: ", e);
+  })
